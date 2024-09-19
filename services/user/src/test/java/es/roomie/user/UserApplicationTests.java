9c75d039247a3f9a9710d9936240dbca8e2077e4
TitoMunozAbreu <97183342+TitoMunozAbreu@users.noreply.github.com>
2024-09-19 08:13:22 +0200

    created user-service

---
+++ services/user/src/test/java/es/roomie/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package es.roomie.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
