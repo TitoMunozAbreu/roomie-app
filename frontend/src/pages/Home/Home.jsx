c8f35047fea9bc19ab7f6618b86ce27203f36187
TitoMunozAbreu <97183342+TitoMunozAbreu@users.noreply.github.com>
2024-09-28 07:53:17 +0200

    updated feature

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { Row, Col, Button, Card } from "antd";
-import "./Home.css";
-import cleanHouse from "../../assets/images/clean-house.jpg";
-import historyClass from "../../assets/images/history-task.jpg";
-import smartIA from "../../assets/images/smart-ia.jpg";
-import organizerTask from "../../assets/images/organizer-task.jpg";
+import { Divider } from "antd";
 
+import "./Home.css";
+
+import cleanHouse from "../../assets/images/clean-house.jpg";
+import { FEATURES } from "./fetures";
 
 export default function Home() {
   return (
     <div className="home-page">
-      {/* Sección 1: Imagen derecha y texto izquierdo */}
+        
+      {/* Section 1: rigth image and left some text*/}
       <section className="intro-section">
         <Row gutter={16} align="middle">
-          {/* Columna izquierda - Texto */}
           <Col xs={24} md={12}>
             <h1>Manage Your Household Tasks</h1>
             <p>
-            Organize and assign tasks easily to save time and simplify your life.
+              Organize and assign tasks easily to save time and simplify your
+              life.
             </p>
             <Button type="primary" size="large">
               Sign in
             </Button>
           </Col>
-          {/* Columna derecha - Imagen */}
+
           <Col xs={24} md={12}>
             <img
               src={cleanHouse}
@@ -34,58 +35,25 @@ export default function Home() {
         </Row>
       </section>
 
-      {/* Sección 2: Tres características principales */}
+      {/* Section 2: Three main features */}
       <section className="features-section">
-        <h2>Características Principales</h2>
+        <Divider plain>
+          <h2>Main Features</h2>
+        </Divider>
         <Row gutter={16}>
-          {/* Característica 1 */}
-          <Col xs={24} md={8}>
-            <Card
-              hoverable
-              cover={
-                <img alt="Organización" src={organizerTask} />
-              }
-            >
-              <Card.Meta
-                title="Task Organization"
-                description="Organize cleaning tasks and assign responsibilities easily."
-              />
-            </Card>
-          </Col>
-          {/* Característica 2 */}
-          <Col xs={24} md={8}>
-            <Card
-              hoverable
-              cover={
-                <img
-                  alt="Asignación Inteligente"
-                  src={smartIA}
-                />
-              }
-            >
-              <Card.Meta
-                title="Smart Assignment"
-                description="Use our artificial intelligence to distribute tasks fairly."
-              />
-            </Card>
-          </Col>
-          {/* Característica 3 */}
-          <Col xs={24} md={8}>
-            <Card
-              hoverable
-              cover={
-                <img
-                  alt="Historial de Tareas"
-                  src={historyClass}
-                />
-              }
-            >
-              <Card.Meta
-                title="Task History"
-                description="Check the history of completed tasks and optimize your time."
-              />
-            </Card>
-          </Col>
+          {FEATURES.map(({ key, title, description, image }) => (
+            <Col xs={24} md={8}>
+              <Card
+                key={key}
+                hoverable
+                cover={
+                  <img className="feature-image" alt={title} src={image} />
+                }
+              >
+                <Card.Meta title={title} description={description} />
+              </Card>
+            </Col>
+          ))}
         </Row>
       </section>
     </div>
