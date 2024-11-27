7a8d0a60d931fbbea023ac699284e69fa6cc60c4
TitoMunozAbreu <97183342+TitoMunozAbreu@users.noreply.github.com>
2024-11-27 11:25:13 +0100

    updated validation requests

--- backend/services/household/src/main/java/es/roomie/household/exceptions/GloblalExceptionHandler.java
+++ backend/services/household/src/main/java/es/roomie/household/exceptions/GloblalExceptionHandler.java
@@ -5,9 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -68,5 +72,18 @@ public class GloblalExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<?> handlerBadRequestException(MissingServletRequestParameterException ex) {
         return new ResponseEntity<>(ex.getMessage(), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        HashMap<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return new ResponseEntity<>(errors, BAD_REQUEST);
     }
 }
